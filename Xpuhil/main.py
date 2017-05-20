42a94a0391eb3c12d0e84a601832e3aefa325dc0
Abraham arce <xe1gyq@gmail.com>
2017-05-20 16:27:28 +0000

    Update
    Signed-off-by: Abraham arce <xe1gyq@gmail.com>

---
+++ Xpuhil/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import atexit
+import dweepy
+import signal
+import sys
+import time
+
+from upm import pyupm_grove as grove
+
+light = grove.GroveLight(0)
+
+dweetiodatasource = {}
+dweetiothingname = "ilab_xpuhil"
+
+def SIGINTHandler(signum, frame):
+	raise SystemExit
+
+def exitHandler():
+	print "Exiting"
+        time.sleep(2)
+        dweetiodatasource['alive'] = "0"
+        dweetiodatasource['luxes'] =  0
+        dweetiodatasource['message'] = "None"
+        dweepy.dweet_for(dweetiothingname, dweetiodatasource)
+	sys.exit(0)
+
+atexit.register(exitHandler)
+signal.signal(signal.SIGINT, SIGINTHandler)
+
+if __name__ == '__main__':
+
+    message = "Hello Xpuhil!"
+
+    while True:
+
+        luxes = light.value()
+
+        dweetiodatasource['alive'] = "1"
+        dweetiodatasource['luxes'] =  luxes
+        dweetiodatasource['message'] = message
+        dweepy.dweet_for(dweetiothingname, dweetiodatasource)
+
+        time.sleep(2)
